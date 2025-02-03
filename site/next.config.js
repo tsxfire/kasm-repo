e22f4212e1385050af8d7f5e1e63877bb473d245
tsxfire <32419215+tsxfire@users.noreply.github.com>
2025-02-03 13:11:08 -0600

    asdf

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'NerdsWork Registry',
+    description: 'The official store for NerdsWork supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/tsxfire/kasm-repo/',
+    contactUrl: 'https://nerdswork.net/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-repo/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
