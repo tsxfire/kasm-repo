a8bbdeb78cf2cb6356b5aaf52d16f318d5924897
tsxfire <32419215+tsxfire@users.noreply.github.com>
2025-02-03 15:19:04 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'NerdsWork Registry',
     description: 'The official store for NerdsWork supported workspaces.',
     icon: 'https://tsxfire.github.io/kasm-repo/1.1/NerdsworkLogo.png',
-    listUrl: 'https://github.com/tsxfire/kasm-repo/',
+    listUrl: 'https://tsxfire.github.io/kasm-repo/',
     contactUrl: 'https://nerdswork.net/support',
   },
   reactStrictMode: true,
