c96f3df552f67ed486e4077bc29f6c8a108c9df0
tsxfire <32419215+tsxfire@users.noreply.github.com>
2025-02-03 14:33:38 -0600

    updatedlogo

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'NerdsWork Registry',
     description: 'The official store for NerdsWork supported workspaces.',
-    icon: '/img/logo.svg',
+    icon: '\site\public\NerdsworkLogo.png',
     listUrl: 'https://github.com/tsxfire/kasm-repo/',
     contactUrl: 'https://nerdswork.net/support',
   },
